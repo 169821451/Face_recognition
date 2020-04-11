893b562f023cdb5006c95eb38ed9ba94ce784f0d
169821451 <169821451@qq.com>
2020-04-12 00:54:43 +0800

    虹软识别引擎初始化完成

---
+++ widget.cpp
@@ -0,0 +1,80 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+
+
+
+
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+
+
+//    char* APPID = "8s22FzspXbsRQHoE9a4AAAUPUJN2DNixFEhtntPTSQnq";
+//    char* SDKKey = "3ibZEtk5MdMAzBtTLHxUzwR1A6DGghS37UmEqWbk3i98";
+    char APPID[] = "8s22FzspXbsRQHoE9a4AAAUPUJN2DNixFEhtntPTSQnq";
+    char* appID = APPID;
+    char SDKKey[] = "3ibZEtk5MdMAzBtTLHxUzwR1A6DGghS37UmEqWbk3i98";
+    char* sdkKey = SDKKey;
+    // 激活虹软 SDK
+    MRESULT res = ASFOnlineActivation(APPID,SDKKey);
+    if(MOK != res && MERR_ASF_BASE_ALREADY_ACTIVATED != res)
+    {
+        printf("ALActivation fail: %d\n", res);
+
+        return;
+    }else
+    {
+        printf("ALActivation sucess: %d\n", res);
+    }
+    // 初始化识别引擎
+    MHandle handle = NULL;
+    MInt32 nScale = 32;
+    MInt32 faceNum = 5;
+    MInt32 mask = ASF_FACE_DETECT | ASF_FACERECOGNITION | ASF_AGE | ASF_GENDER | ASF_FACE3DANGLE;
+    // 引擎初始化参数一为设置为视频模式
+    MRESULT InitRes = ASFInitEngine(ASF_DETECT_MODE_VIDEO,ASF_OP_0_ONLY,16,5,mask,&handle);
+    if(InitRes != MOK)
+        QMessageBox::warning(nullptr,"错误","初始化失败！",QMessageBox::Ok);
+    else 
+        QMessageBox::information(nullptr,"成功","初始化成功!",QMessageBox::Ok);
+
+    // 人脸检测
+    VideoCapture cap;
+    cap.open("F:/CPPPritice/VS_QT/Face_recognition/1.mp4");
+
+
+    
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+    // 点击加载视频
+
+    namedWindow("video",WINDOW_AUTOSIZE);
+
+    VideoCapture cap;
+    cap.open("F:/CPPPritice/VS_QT/Face_recognition/1.mp4");
+    Mat frame;
+    while (1)
+    {
+        cap >> frame;
+        if(frame.empty())
+            break;
+        imshow("video",frame);
+        if(waitKey(33) >= 0)
+            break;
+
+    }
+
+}
---
+++ widget.h
@@ -0,0 +1,57 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+// 设置中文
+#if _MSC_VER >= 1600
+#pragma execution_character_set("utf-8")
+#endif
+
+#include <QWidget>
+#include <opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <arcsoft_face_sdk.h>
+#include <Windows.h>
+#include <iostream>
+#include <stdio.h>
+#include <merror.h>
+#include <amcomdef.h>
+#include <QMessageBox>
+
+
+#pragma comment(lib, "libarcsoft_face_engine.lib")
+//#define APPID "8s22FzspXbsRQHoE9a4AAAUPUJN2DNixFEhtntPTSQnq"
+//#define SDKKey "3ibZEtk5MdMAzBtTLHxUzwR1A6DGghS37UmEqWbk3i98"
+#define MERR_ASF_BASE_ALREADY_ACTIVATED 90114 //SDK已激活
+#define SafeFree(p) { if ((p)) free(p); (p) = NULL; }
+#define SafeArrayDelete(p) { if ((p)) delete [] (p); (p) = NULL; }
+#define SafeDelete(p) { if ((p)) delete (p); (p) = NULL; }
+
+
+
+
+using namespace std;
+using namespace cv;
+
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::Widget *ui;
+};
+
+#endif // WIDGET_H
